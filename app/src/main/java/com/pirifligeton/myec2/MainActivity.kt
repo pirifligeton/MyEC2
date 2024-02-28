a91ec3445d2bd0ebe9182e2ddafd123f1bee9e32
pirifligeton <pirifligeton@gmail.com>
2024-02-27 21:20:12 -0500

    design only commit

--- app/src/main/java/com/pirifligeton/myec2/MainActivity.kt
+++ app/src/main/java/com/pirifligeton/myec2/MainActivity.kt
@@ -2,20 +2,13 @@ package com.pirifligeton.myec2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.pirifligeton.myec2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 
-    override fun onClick(v: View?) {
-        startActivity(Intent(applicationContext, RegistrationActivity::class.java))
-    }
+    
 }
